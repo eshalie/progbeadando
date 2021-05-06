b9df3edfe403c4e6fbad6b7439aecd7573b9dd35
eshalie <eshalie01@gmail.com>
2021-05-06 21:25:39 +0200

    Asd

--- 23.feladat.py
+++ 23.feladat.py
@@ -12,6 +12,67 @@ import random
 import itertools
 
 
+
+def eredmeny(csapatok):
+    eredmenyek={}
+    for i in range(0,len(csapatok)-1):
+            for j in range(i+1,len(csapatok)):
+                gol1 = str(random.randint(0,4))
+                gol2= str(random.randint(0,4))
+                eredmenyek[f'{csapatok[i]} {csapatok[j]}'] = f'{gol1}:{gol2}'
+    return eredmenyek
+
+
+def pontszamitas(eredmenyek):
+    pontok=np.zeros(n)
+    rugottgol=np.zeros(n)
+    kapottgol=np.zeros(n)
+
+    for i in range(len(csapatok)):
+        for kulcs, ertek in eredmenyek.items():
+            kulcs=kulcs.split()
+            ertek=ertek.split(':')
+            otthoni=kulcs[0]
+            otthonigol=int(ertek[0])
+            vendeg=kulcs[1]
+            vendeggol=int(ertek[1])
+            if kulcs[i]==otthoni:
+                if otthonigol>vendeggol:
+                    pontok[i]+=3
+                elif otthonigol==vendeggol:
+                    pontok[i]+=1
+                rugottgol[i]+=otthonigol
+                kapottgol[i]+=vendeggol
+            elif csapatok[i]==vendeg:
+                if otthonigol<vendeggol:
+                    pontok[i]+=3
+                elif otthonigol==vendeggol:
+                    pontok[i]+=1
+                rugottgol[i]+=vendeggol
+                kapottgol[i]+=otthonigol
+    return pontok, rugottgol, kapottgol
+
+
+def allas(pontok,rugottgolok,kapottgolok,csapatok):
+    for i in range(0,len(pontok)-1):
+        for j in range(i+1,len(pontok)):
+            if pontok[i] < pontok[j]:
+                pontok[i], pontok[j] = pontok[j], pontok[i]
+                rugottgolok[i], rugottgolok[j] = rugottgolok[j], rugottgolok[i]
+                kapottgolok[i], kapottgolok[j] = kapottgolok[j], kapottgolok[i]
+                csapatok[i], csapatok[j] = csapatok[j], csapatok[i]
+
+            elif pontok[i] == pontok[j]:
+                if rugottgolok[j] > rugottgolok[i]:
+                    pontok[i], pontok[j] = pontok[j], pontok[i]
+                    rugottgolok[i], rugottgolok[j] = rugottgolok[j], rugottgolok[i]
+                    kapottgolok[i], kapottgolok[j] = kapottgolok[j], kapottgolok[i]
+                    csapatok[i], csapatok[j] = csapatok[j], csapatok[i]
+    print("Pontállás:")
+    print("Helyezés Név Pont Rúgott gólok Kapott gólok",i,csapatok[i],rugottgolok[i],kapottgolok[i])
+
+
+
 while True:
     n=input("Adjon meg egy számot:")
     if n.isnumeric():   #ellenőrizzük, hogy az input szám e
@@ -22,6 +83,8 @@ while True:
             break
     else:
         print("Kérem számot adjon meg")
+
+
 
 csapatok=[""]
 csapatok[0]=input("Adjon meg egy csapat nevet ")
@@ -37,12 +100,5 @@ while True:
         if db==n:
             break
 
-csapat_parok=list(itertools.combinations(csapatok, 2))      #megalkotjuk a csapatpárokat
-rand=np.random.randint(0,10,n)
-eredmenyek=list(itertools.combinations(rand, 2))
-random.shuffle(eredmenyek)
-"""pontok=[]
-for i in range(len(csapatok)):
-    if eredmenyek[i]:"""
-print(csapatok,csapat_parok, rand, eredmenyek)
-
+pontok, rugottgol , kapottgol=pontszamitas(eredmeny(csapatok))
+allas(pontok,rugottgol,kapottgol,csapatok)
