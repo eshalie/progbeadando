4a72d54578c80f8678f7825da704b6c87384cc0e
eshalie <eshalie01@gmail.com>
2021-05-06 17:12:18 +0200

    asd

---
+++ 23.feladat.py
@@ -0,0 +1,8 @@
+"""Írjon programot, amely egy labdarúgó bajnokság meccseit képes legenerálni, és ez alapján
+egy rangsort kialakítani a csapatok között. A program először kérjen be egy n számot, majd
+pedig n darab csapat nevét. Ezt követően pedig random generálja le a csapatok egymás
+elleni meccseinek eredményeit (mindenki játsszon mindenkivel elven), amit a pálda
+kimenet alapján kiír a program. A program végül készítsen egy eredménytáblát a meccsek
+eredményei alapján (lásd példa kimenet). Pontozás: győzelem: 3 pont, döntetlen: 1 pont,
+vereség: 0pont. Ha két csapat azonos pontszámmal végez, akkor közöttük a gólkülönbség
+(rúgott gólok – kapott gólok) rangsorol."""
