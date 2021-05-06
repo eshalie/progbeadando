c1c40900b37f8687455adb83057e2c0c367c0f61
eshalie <eshalie01@gmail.com>
2021-05-06 17:17:59 +0200

    asd

---
+++ 49.feladat.py
@@ -0,0 +1,8 @@
+"""Készítsen egy programot amely megkapja egy termék összetevőit szövegként. A program
+ismerje fel a legfontosabb allergéneket (https://csakamentes.hu/14-allergen-reszletesen/).
+Mentse le ezen allergén anyagoknak az előfordulási számait (a szövegben hányszor
+szerepelnek) és jelenítse meg könnyen áttekinthető formában, matplotlib segítségével (pl.
+oszlopdiagram). Alakítsa ezek után úgy a programot, hogy annak file-ként meg lehet adni a
+beolvasandó szöveget és ki lehet választani mely allergénekre vagyunk érzékenyek. Ezeket a
+matplotlib ábrázoláson jelölje külön (pl. színezéssel) és mentse le egy kimeneti mappába az
+elemzés eredményét"""
