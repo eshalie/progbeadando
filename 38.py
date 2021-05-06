8cd5e67cd4a34baeb22ea51347fc0679585380aa
eshalie <eshalie01@gmail.com>
2021-05-06 17:15:47 +0200

    asd

---
+++ 38.py
@@ -0,0 +1,3 @@
+"""Készítsen programot amely az input.txt-ből számhármasokat olvas be (k, l, m), ahol k egy
+l-es számrendszerben megadott szám amit m-es számrendszerbe vált át. Végül az
+output.txt-be írja az átváltott értékeket a 10 számrendszerbeli értékükkel együtt."""
