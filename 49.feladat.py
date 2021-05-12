501cc4af9a5fd7c9a98e95656938e28db6fc99eb
eshalie <eshalie01@gmail.com>
2021-05-12 12:33:21 +0200

    allergének számlálása

--- 49.feladat.py
+++ 49.feladat.py
@@ -6,3 +6,27 @@ oszlopdiagram). Alakítsa ezek után úgy a programot, hogy annak file-ként meg
 beolvasandó szöveget és ki lehet választani mely allergénekre vagyunk érzékenyek. Ezeket a
 matplotlib ábrázoláson jelölje külön (pl. színezéssel) és mentse le egy kimeneti mappába az
 elemzés eredményét"""
+
+import numpy as np
+
+def szamlalo(file,allergen):
+    tmp=len(allergen)
+    szamlalo=np.zeros(tmp,dtype=int)
+    for i in range(len(file)):
+        for j in range(len(allergen)):
+            if allergen[j] in file[i]:
+                szamlalo[j]+=1
+    return szamlalo
+
+try:
+    ifile = open("input2.txt","r")
+    afile=open("allergen.txt","r")
+    allergenek=afile.read().lower().split(", ")
+    osszetevok=ifile.read().lower().split(", ")
+    szaml=szamlalo(osszetevok,allergenek)
+    print(allergenek)
+    print(szaml)
+    afile.close()
+    ifile.close()
+except FileNotFoundError:
+    print("input2.txt or allergen.txt nem letezik")
