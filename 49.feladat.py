e7607e3b8cc2ed8bcb30366f67b27ad55ec8d745
eshalie <eshalie01@gmail.com>
2021-05-12 14:22:09 +0200

    Befejezett munka

--- 49.feladat.py
+++ 49.feladat.py
@@ -8,6 +8,7 @@ matplotlib ábrázoláson jelölje külön (pl. színezéssel) és mentse le egy
 elemzés eredményét"""
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def szamlalo(file,allergen):
     tmp=len(allergen)
@@ -18,14 +19,33 @@ def szamlalo(file,allergen):
                 szamlalo[j]+=1
     return szamlalo
 
+def sajatallergia(sall,allergenek,szaml):
+    tmp = len(allergenek)
+    sajatallergia = np.zeros(tmp, dtype=int)
+    for i in range(len(sajatallergia)):
+        for j in range(len(sall)):
+            if szaml[i]!=0 and sall[j] in allergenek[i]:
+                sajatallergia[i]=1
+    return sajatallergia
+
 try:
-    ifile = open("input2.txt","r")
-    afile=open("allergen.txt","r")
+    ifile = open("input2.txt","r",encoding='utf-8')
+    afile=open("allergen.txt","r",encoding='utf-8')
     allergenek=afile.read().lower().split(", ")
     osszetevok=ifile.read().lower().split(", ")
+    sall=input("Kérem adja meg allergéneit vesszővel elválasztva: ")
+    sall=sall.lower().split(",")
     szaml=szamlalo(osszetevok,allergenek)
-    print(allergenek)
-    print(szaml)
+    sallsz=sajatallergia(sall,allergenek,szaml)
+    oszlopok=plt.bar(allergenek, szaml, color="Blue")
+    plt.xticks(fontsize=10, rotation=45)
+    plt.xlabel("Allergének")
+    plt.ylabel("Előfordulás")
+    for i in range(len(sallsz)):
+        if sallsz[i]==1:
+            oszlopok[i].set_color('r')
+    plt.savefig("49/kutatas.png")
+    plt.show()
     afile.close()
     ifile.close()
 except FileNotFoundError:
